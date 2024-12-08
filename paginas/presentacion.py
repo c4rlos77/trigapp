b1a869d236c670c81a0cff947746f22cf33ac7bd
1010962348carlos <1010962348carlos@gmail.com>
2024-12-08 17:43:37 +0000

    m

--- paginas/presentacion.py
+++ paginas/presentacion.py
@@ -1,19 +1,11 @@
 import streamlit as st
 
-# Configuración de la página
-# st.set_page_config(page_title="Presentación Personal", layout="wide")
-
-# Título de la página
 st.markdown("<h1 style='text-align: center; color: #000000;'>Mi Presentación Personal</h1>", unsafe_allow_html=True)
 
-# Layout con dos columnas
 col1, col2 = st.columns([1, 2])
-
-# Columna 1: Foto del estudiante
 with col1:
     st.image("https://scontent.fbga1-3.fna.fbcdn.net/v/t39.30808-6/465027005_8474547919308693_5979528301396813541_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGqIwIVkRfSHJ49oFlx-j6sH2VxEHUpkmYfZXEQdSmSZs56r-hb-vWcnart9DRICtDfIqFIC08V-Jj_L3aoC80a&_nc_ohc=LmGIEmLd4VUQ7kNvgFYLfuc&_nc_zt=23&_nc_ht=scontent.fbga1-3.fna&_nc_gid=Ah2fGnqypRdWKZKeIzklcnS&oh=00_AYAS2_D3I-CtvVTaJ3FQZMqNnf5mnYwTqJ-nNyDTOVz-xw&oe=675B7A5A", caption="Holi, soy Carlos ")
 
-# Columna 2: Texto de introducción
 with col2:
     st.markdown("""
     Soy estudiante de Matemáticas Puras de la **Universidad Industrial de Santander (UIS)**.  
@@ -25,7 +17,6 @@ with col2:
     jugando ajedrez y manteniéndome activo con el deporte.
     """)
 
-# Marco 1: Intereses y afinidades
 st.markdown("<hr>", unsafe_allow_html=True)
 with st.container():
     st.markdown("<h2 style='text-align: center; color: #000000;'>Mis intereses</h2>", unsafe_allow_html=True)
@@ -35,7 +26,6 @@ with st.container():
     seguir descubriendo más ramas matemáticas que me ayuden a crecer como científico.
     """)
 
-# Marco 2: Reflexión sobre Programación I
 st.markdown("<hr>", unsafe_allow_html=True)
 with st.container():
     st.markdown("<h2 style='text-align: center; color: #000000;'>Programación I</h2>", unsafe_allow_html=True)
@@ -45,7 +35,6 @@ with st.container():
     if st.button("Ver proyectos realizados"):
         st.write("Durante el desarrollo de todo el curso, he creado bloques de código para resolver problemas específicos, como desafíos matemáticos, lógicos y de análisis de datos, entre otros. El único proyecto grande, por así decirlo, es la página que acabas de ver. Sin embargo, me interesa adquirir más proyectos que pongan a prueba mis conocimientos y habilidades lógicas.")
 
-# Footer interactivo
 st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>Gracias por visitar mi presentación 😊</h3>", unsafe_allow_html=True)
 if st.button("Conectar conmigo"):
